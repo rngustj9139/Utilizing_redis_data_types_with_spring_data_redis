6a618885425f3ab788bb03a5da90ce1965171937
rngustj9139 <mnb9139@khu.ac.kr>
2024-03-07 16:04:32 +0900

    First commit

---
+++ src/main/java/koo/spring_data_redis_utilization/SpringDataRedisUtilizationApplication.java
@@ -0,0 +1,13 @@
+package koo.spring_data_redis_utilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisUtilizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisUtilizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/spring_data_redis_utilization/SpringDataRedisUtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.spring_data_redis_utilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisUtilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

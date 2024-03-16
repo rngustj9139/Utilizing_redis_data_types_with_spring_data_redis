c843bec112f96042ae049011e7cb2fda6d44d79c
rngustj9139 <mnb9139@khu.ac.kr>
2024-03-17 00:15:31 +0900

    Add:ShoppingCartRedisRepository

---
+++ src/main/java/koo/shoppingCart/repository/ShoppingCartRedisRepository.java
@@ -0,0 +1,34 @@
+package koo.shoppingCart.repository;
+
+import koo.shoppingCart.domain.entity.Item;
+import koo.spring_data_redis_utilization.snsActivityFeed.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+@RequiredArgsConstructor
+public class ShoppingCartRedisRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Long add(User user, Item item) {
+        Long result = redisTemplate
+                .opsForSet()
+                .add("user:" + Long.toString(user.getId()) + ":cart", "item:" + Long.toString(item.getId()));// SADD
+
+        return result;
+    }
+
+    public Set<String> get(User user) {
+        Set<String> result = redisTemplate
+                .opsForSet()
+                .members("user:" + Long.toString(user.getId()) + ":cart");// SMEMBERS
+
+        return result;
+    }
+
+}

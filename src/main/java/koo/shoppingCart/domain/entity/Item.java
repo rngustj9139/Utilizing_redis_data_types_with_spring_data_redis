96987b843113b366888467782c51bd2b0cf5e5e4
rngustj9139 <mnb9139@khu.ac.kr>
2024-03-16 23:01:16 +0900

    Update:User, Item

--- src/main/java/koo/shoppingCart/domain/entity/Item.java
+++ src/main/java/koo/shoppingCart/domain/entity/Item.java
@@ -1,10 +1,13 @@
 package koo.shoppingCart.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@Getter @Setter
 public class Item {
 
     @Id
@@ -15,5 +18,11 @@ public class Item {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    //== 연관관계 편의 메서드 ==//
+    public void setUser(User user) {
+        this.user = user;
+        user.getItems().add(this);
+    }
 
 }
--- src/main/java/koo/shoppingCart/domain/entity/User.java
+++ src/main/java/koo/shoppingCart/domain/entity/User.java
@@ -1,13 +1,16 @@
 package koo.shoppingCart.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
+@Getter @Setter
 public class User {
 
     @Id
